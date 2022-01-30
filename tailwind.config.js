7a8d5fafd969ef8d1d8cc5aea2495dd902ed7d7e
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-30 20:24:55 +0600

    tailwind container config cunstomize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    sm: '2rem',
+                    lg: '4rem',
+                    xl: '6rem',
+                    '2xl': '13rem',
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
