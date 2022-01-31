e16688e580c4afe10ed9010a910f774eb9220eee
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-31 21:57:28 +0600

    regular post design complete

---
+++ src/components/RegularPost.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+    imgSrc: string;
+    title: string;
+    text: string;
+    author: string;
+    postCategory: string;
+    date: string;
+    authorUrl: string;
+    categoryUrl: string;
+}
+
+const RegularPost = ({
+    imgSrc,
+    title,
+    text,
+    author,
+    postCategory,
+    date,
+    authorUrl,
+    categoryUrl,
+}: Props) => (
+    <div className="flex">
+        {/* image container */}
+        <div className="relative flex-1">
+            <Image src={imgSrc} layout="fill" objectFit="cover" />
+        </div>
+
+        {/* content */}
+        <div className="flex-[2] px-[20px] py-[10px] flex flex-col gap-2">
+            {/* title */}
+            <h2 className="text-[14px] font-medium">{title}</h2>
+            {/* text */}
+            <p className="text-[0.85rem] text-gray-600 font-light mb-2">{text}</p>
+            {/* meta data */}
+            <div className="flex flex-col gap-2 text-[0.8rem] text-[#b4b4b4]">
+                <div>
+                    {/* author name */}
+                    <Link href={authorUrl}>
+                        <span className="text-black cursor-pointer">{author}</span>
+                    </Link>
+                    {' in '}
+                    {/* post category */}
+                    <Link href={categoryUrl}>
+                        <span className="text-black cursor-pointer">{postCategory}</span>
+                    </Link>
+                </div>
+                <span>{date}</span>
+            </div>
+        </div>
+    </div>
+);
+
+export default RegularPost;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // npm install html-react-parser moment react-multi-carousel sass
 import Head from 'next/head';
 import FeaturedPost from '../components/FeaturedPost';
+import RegularPost from '../components/RegularPost';
 
 export default function Home() {
     return (
@@ -21,6 +22,29 @@ export default function Home() {
                 authorUrl="#"
                 categoryUrl="#"
             />
+
+            {/* posts container */}
+            <div className="container mt-[100px]">
+                {/* section title */}
+                <h2 className="text-xl inline-flex mb-14 border-b border-[#222]">Regular Posts</h2>
+
+                {/* posts */}
+                <div className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-2">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => (
+                        <RegularPost
+                            key={id}
+                            imgSrc="https://preview.colorlib.com/theme/meranda/images/xbig_img_1.jpg.pagespeed.ic.K2N7KNYATl.webp"
+                            title="News Needs to Meet Its Audiences Where They Are"
+                            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam."
+                            author="Dave Rogas"
+                            postCategory="Food"
+                            date="31 January 2022"
+                            authorUrl="#"
+                            categoryUrl="#"
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
