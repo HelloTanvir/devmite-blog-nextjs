622516a84fe9040244b8ee64de275427426b36be
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-02-01 10:40:28 +0600

    posts container component separated

---
+++ src/components/PostsContainer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    sectionTitle: string;
+}
+
+const PostsContainer: React.FC<Props> = ({ sectionTitle, children }) => (
+    <div className="container mt-[100px]">
+        {/* section title */}
+        <h2 className="text-xl inline-flex mb-14 border-b border-[#222]">{sectionTitle}</h2>
+
+        {/* posts */}
+        <div className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-2">{children}</div>
+    </div>
+);
+
+export default PostsContainer;
