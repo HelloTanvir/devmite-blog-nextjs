732201c91d2b4dc607e3dfc828b4f15f0cf85732
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-02-01 15:50:23 +0600

    data modified

--- src/data/editorsPickedPosts.ts
+++ src/data/editorsPickedPosts.ts
@@ -8,11 +8,11 @@ const editorsPickedPosts = [
         date: '31 January 2022',
         authorUrl: '#',
         categoryUrl: '#',
-        isHighlightedPost: true,
     },
     {
         imgSrc: 'https://preview.colorlib.com/theme/meranda/images/xbig_img_1.jpg.pagespeed.ic.K2N7KNYATl.webp',
         title: 'News Needs to Meet Its Audiences Where They Are',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.',
         author: 'Dave Rogas',
         postCategory: 'Food',
         date: '31 January 2022',
@@ -22,6 +22,7 @@ const editorsPickedPosts = [
     {
         imgSrc: 'https://preview.colorlib.com/theme/meranda/images/xbig_img_1.jpg.pagespeed.ic.K2N7KNYATl.webp',
         title: 'News Needs to Meet Its Audiences Where They Are',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.',
         author: 'Dave Rogas',
         postCategory: 'Food',
         date: '31 January 2022',
@@ -31,6 +32,7 @@ const editorsPickedPosts = [
     {
         imgSrc: 'https://preview.colorlib.com/theme/meranda/images/xbig_img_1.jpg.pagespeed.ic.K2N7KNYATl.webp',
         title: 'News Needs to Meet Its Audiences Where They Are',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.',
         author: 'Dave Rogas',
         postCategory: 'Food',
         date: '31 January 2022',
