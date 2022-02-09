74983a2321eb69a90a33311bb73792782d7134a6
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-02-09 12:25:41 +0600

    fields for registration

---
+++ src/utils/RegistrationFields.ts
@@ -0,0 +1,28 @@
+const RegistrationFields = [
+    {
+        label: 'Name',
+        type: 'text',
+        name: 'name',
+        placeholder: 'Enter your name',
+    },
+    {
+        label: 'Address',
+        type: 'text',
+        name: 'address',
+        placeholder: 'Enter your address',
+    },
+    {
+        label: 'Email',
+        type: 'text',
+        name: 'email',
+        placeholder: 'Enter your email address',
+    },
+    {
+        label: 'Password',
+        type: 'password',
+        name: 'password',
+        placeholder: 'Enter your password',
+    },
+];
+
+export default RegistrationFields;
