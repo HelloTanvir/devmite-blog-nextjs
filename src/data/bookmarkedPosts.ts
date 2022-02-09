6a3e307ca29d7a73febf06fa35c959b62ed1709d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-02-09 17:34:37 +0600

    fake bookmark posts created

---
+++ src/data/bookmarkedPosts.ts
@@ -0,0 +1,36 @@
+const bookmarkedPosts = [
+    {
+        title: 'News Needs to Meet Its Audiences Where They Are',
+        author: 'Dave Rogers',
+        postCategory: 'News',
+        date: '31 January 2022',
+        authorUrl: '#',
+        categoryUrl: '#',
+    },
+    {
+        title: 'News Needs to Meet Its Audiences Where They Are',
+        author: 'Dave Rogers',
+        postCategory: 'News',
+        date: '31 January 2022',
+        authorUrl: '#',
+        categoryUrl: '#',
+    },
+    {
+        title: 'News Needs to Meet Its Audiences Where They Are',
+        author: 'Dave Rogers',
+        postCategory: 'News',
+        date: '31 January 2022',
+        authorUrl: '#',
+        categoryUrl: '#',
+    },
+    {
+        title: 'News Needs to Meet Its Audiences Where They Are',
+        author: 'Dave Rogers',
+        postCategory: 'News',
+        date: '31 January 2022',
+        authorUrl: '#',
+        categoryUrl: '#',
+    },
+];
+
+export default bookmarkedPosts;
