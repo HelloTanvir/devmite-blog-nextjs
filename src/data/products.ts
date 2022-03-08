5247efb3b8e60ba410cdd1c6add8e94034fc1ece
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-03-08 10:18:47 +0600

    fake products data

---
+++ src/data/products.ts
@@ -0,0 +1,94 @@
+const products = [
+    {
+        id: 1,
+        name: 'test 1',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 2,
+        name: 'test 2',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 3,
+        name: 'test 3',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 4,
+        name: 'test 4',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 5,
+        name: 'test 5',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 6,
+        name: 'test 6',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 7,
+        name: 'test 7',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 8,
+        name: 'test 8',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 9,
+        name: 'test 9',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 10,
+        name: 'test 10',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 11,
+        name: 'test 11',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 12,
+        name: 'test 12',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 13,
+        name: 'test 13',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 14,
+        name: 'test 14',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+    {
+        id: 15,
+        name: 'test 15',
+        price: 100,
+        image: 'https://i.pinimg.com/564x/c7/f6/bd/c7f6bdef61e2558147339c0057eee2e8.jpg',
+    },
+];
+
+export default products;
