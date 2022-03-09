4c1c9b23500616211547a78618475c68882fe728
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-03-09 22:57:05 +0600

    cart items count in cart button

--- src/components/IconSet.tsx
+++ src/components/IconSet.tsx
@@ -7,6 +7,8 @@ import {
     // eslint-disable-next-line prettier/prettier
     RiUser6Fill
 } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 import SearchBar from './SearchBar';
 import ThemeSwitch from './ThemeSwitch';
 
@@ -14,34 +16,40 @@ interface Props {
     className?: string;
 }
 
-const IconSet = ({ className }: Props) => (
-    <div className={className}>
-        <SearchBar />
+const IconSet = ({ className }: Props) => {
+    const cartLength = useSelector<RootState, RootState['cart']>((store) => store.cart).length;
 
-        {/* social links */}
-        <RiFacebookFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
-        <RiTwitterFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
-        <RiInstagramFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
+    return (
+        <div className={className}>
+            <SearchBar />
 
-        {/* theme toggle button */}
-        <ThemeSwitch />
+            {/* social links */}
+            <RiFacebookFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
+            <RiTwitterFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
+            <RiInstagramFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
 
-        {/* cart button */}
-        <Link href="/cart">
-            <button type="button" className="relative">
-                <RiShoppingBasketFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
+            {/* theme toggle button */}
+            <ThemeSwitch />
 
-                <span className="absolute -top-2 -right-2 bg-[#FF7020] text-white rounded-full text-[8px] h-[15px] w-[15px] flex justify-center items-center">
-                    1
-                </span>
-            </button>
-        </Link>
+            {/* cart button */}
+            <Link href="/cart">
+                <button type="button" className="relative">
+                    <RiShoppingBasketFill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
 
-        {/* profile button */}
-        <Link href="/profile">
-            <RiUser6Fill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
-        </Link>
-    </div>
-);
+                    {cartLength > 0 ? (
+                        <span className="absolute -top-2 -right-2 bg-[#FF7020] text-white rounded-full text-[8px] h-[15px] w-[15px] flex justify-center items-center">
+                            {cartLength}
+                        </span>
+                    ) : null}
+                </button>
+            </Link>
+
+            {/* profile button */}
+            <Link href="/profile">
+                <RiUser6Fill className="scale-[1.11] cursor-pointer hover:opacity-60 transition-opacity duration-200" />
+            </Link>
+        </div>
+    );
+};
 
 export default IconSet;
